c7f1a3dbee14b56f1fef24a6267a70b22b60d936
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-04-16 04:18:47 +0700

    Edit

--- src/api/product-in-menu-api.js
+++ src/api/product-in-menu-api.js
@@ -1,18 +1,40 @@
 import { BASE_URL } from "../lib/contants";
 import { ACCESS_TOKEN, refreshToken } from "./auth-api";
+import axios from "axios";
 
-export const getProductsInMenu=async (menuId)=>{
-    try {
-        const res= await fetch(`${BASE_URL}/menus/${menuId}/products-menu`,{
-            headers:{
-                Authorization:`Bearer ${JSON.parse(ACCESS_TOKEN)}`
-            }
-        });
-        const data=await  res.json();
-        if(res.status===200) return data;
-        else if(res.status===401) await refreshToken(ACCESS_TOKEN);
-        return null;
-    } catch (error) {
-        console.error("get products in menu exception",error);
-    }
-}
+export const getProductsInMenu = async (param) => {
+  var CURRENT_USER = JSON.parse(localStorage.getItem("admin"));
+  let url = null;
+  if (param.menuId !== undefined) {
+    url =
+      BASE_URL +
+      "/stores/" +
+      param.storeId +
+      "/products?pageNumber=0" +
+      //   param.pageNumber +
+      "&pageSize=5&menuId=" +
+      param.menuId;
+  }
+  console.log(url);
+
+  var response = await axios.get(url, {
+    headers: { Authorization: `Bearer ${CURRENT_USER.token}` },
+  });
+  //   console.log(response.data);
+  return response.data;
+};
+// export const getProductsInMenu=async (menuId)=>{
+//     try {
+//         const res= await fetch(`${BASE_URL}/menus/${menuId}/products-menu`,{
+//             headers:{
+//                 Authorization:`Bearer ${JSON.parse(ACCESS_TOKEN)}`
+//             }
+//         });
+//         const data=await  res.json();
+//         if(res.status===200) return data;
+//         else if(res.status===401) await refreshToken(ACCESS_TOKEN);
+//         return null;
+//     } catch (error) {
+//         console.error("get products in menu exception",error);
+//     }
+// }
